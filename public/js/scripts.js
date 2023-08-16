24ac4c7e90bc8a2e5b4d3565edcd57ace28d787f
kangkyuseok <rbtjr1117@nate.com>
2023-08-16 20:43:58 +0900

    cncn

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,14 @@
-/*!
-* Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     let contentDisplay = document.querySelector('.content-display');
+
+    //     let links = document.querySelectorAll('.spa-link');
+    //     links.forEach(link => {
+    //         link.addEventListener('click', function(e) {
+    //             e.preventDefault(); // 기본 동작(페이지 전환) 방지
+                
+    //             let src = e.currentTarget.getAttribute('data-src');
+    //             contentDisplay.innerHTML = `<img src="${src}" />`;
+    //         });
+    //     });
+    // });
