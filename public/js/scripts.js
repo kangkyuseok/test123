87a0f16187fe0bebe510da398bc2bd705cc8d59c
kangkyuseok <rbtjr1117@nate.com>
2023-08-17 14:23:40 +0900

    ddddfdf

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let contentDisplay = document.querySelector('.content-display');
 
-    // document.addEventListener('DOMContentLoaded', function() {
-    //     let contentDisplay = document.querySelector('.content-display');
+    let links = document.querySelectorAll('.spa-link');
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // 기본 동작(페이지 전환) 방지
+            
+            let href = e.currentTarget.getAttribute('href');
+            fetch(href)
+                .then(response => response.text())
+                .then(data => {
+                    contentDisplay.innerHTML = data;
+                });
+        });
+    });
+});
 
-    //     let links = document.querySelectorAll('.spa-link');
-    //     links.forEach(link => {
-    //         link.addEventListener('click', function(e) {
-    //             e.preventDefault(); // 기본 동작(페이지 전환) 방지
-                
-    //             let src = e.currentTarget.getAttribute('data-src');
-    //             contentDisplay.innerHTML = `<img src="${src}" />`;
-    //         });
-    //     });
-    // });
+AOS.init();
